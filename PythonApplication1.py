d3ef5686f1ee213214d22a47af481ad76cb58e2b
Snowyblue1 <96891904+Snowyblue1@users.noreply.github.com>
2022-07-31 06:47:38 +0100

    Add files via upload

---
+++ PythonApplication1.py
@@ -0,0 +1,6 @@
+import keyboard
+loop = True
+for loop in True:
+    keyboard.press_and_release('shift+1, w, o, r, k, enter')
+for loop in True:
+    keyboard.press_and_release('shift+1, d, a, i, l ,y enter')
